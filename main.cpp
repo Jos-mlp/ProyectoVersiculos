2a2c141614d90f2522988a7c50ab3b8344538fae
Josue Maldonado <Memejosue2012@hotmail.com>
2022-05-29 23:30:57 -0600

    Proyecto lee el archivo y lo divide en palabras

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::vector;
+
+string texto;
+
+void ComprobarPalabra(string p){
+    cout<<"Palabra: "<<p<<endl;
+}
+
+void SepararTexto(string t){
+  string aux;
+  int i = -1;
+  do{
+    i++;
+    if(t[i] == ' ' || t[i] == '\0'){
+        ComprobarPalabra(aux);
+        aux="";
+    }else{
+        aux=aux+t[i];
+    }
+  } while(t[i] != '\0');
+}
+
+void AbrirArchivo(){
+    string filename("v.txt");
+    vector<string> lines;
+    string line;
+
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "El archivo no se encontro: "
+             << filename << "'" << endl;
+        cout << EXIT_FAILURE;
+    }
+
+    while (getline(input_file, line)){
+        lines.push_back(line);
+    }
+
+
+    for (const auto &i : lines){
+        cout << i << endl;
+        string aux=i;
+        SepararTexto(i);
+    }
+
+
+    input_file.close();
+
+}
+
+int main()
+{
+    AbrirArchivo();
+}
